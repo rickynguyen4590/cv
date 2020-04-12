e3d70d321b5fb88914f1deda43aeb788da6c198d
Ricky Nguyen <ducngan90@gmail.com>
2020-04-12 10:32:56 -0500

    Update new footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {Fragment} from "react";
 import App from 'next/app';
+import Head from 'next/head'
 import "../style/orbit.scss"
 
 class CVApp extends App {
     render() {
         const {Component, pageProps} = this.props;
         return (
-            <Component {...pageProps} />
+            <Fragment>
+                <Head>
+                    <title>Ricky Nguyễn</title>
+                </Head>
+                <Component {...pageProps} />
+            </Fragment>
         );
     }
 }
