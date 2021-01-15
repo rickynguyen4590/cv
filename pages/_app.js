f76e55411f7b245546ebf1bd43036bbb09dade24
Ricky Nguyen <ricky.nguyen@noria.no>
2021-01-15 18:03:58 +0700

    Update meta data

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,35 @@ import "../style/orbit.scss"
 
 class CVApp extends App {
     render() {
+        const title = 'Ricky Nguyễn';
+        const site = 'https://cv.kikiguru.com';
+        const description = "A senior full-stack developer with 8 years of work experience in software development"
+        const avatar = 'https://s.gravatar.com/avatar/5a2181af432f7e87e8d505a17dd5254b?s=180';
         const {Component, pageProps} = this.props;
         return (
             <Fragment>
                 <Head>
-                    <title>Ricky Nguyễn</title>
+                    <title>{title}</title>
+                    <meta name="description" content={description}/>
+                    <meta property="og:site_name" content={title}/>
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:title" content={title}/>
+                    <meta property="og:description" content="Senior full-stack developer"/>
+                    <meta property="og:url" content={site}/>
+                    <meta property="og:image"
+                          content={avatar}/>
+                    <meta name="twitter:card" content="summary_large_image"/>
+                    <meta name="twitter:title" content={title}/>
+                    <meta name="twitter:description" content={description}/>
+                    <meta name="twitter:url" content={site}/>
+                    <meta name="twitter:image"
+                          content={avatar}/>
+                    <meta name="twitter:site" content={site}/>
+                    <link rel="canonical" href={site}/>
+                    <meta
+                        name="viewport"
+                        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+                    />
                 </Head>
                 <Component {...pageProps} />
             </Fragment>
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,13 @@
-import Document, {Head, Main, NextScript} from 'next/document';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
 import React from 'react';
 
 class MyDocument extends Document {
     render() {
         return (
-            <html lang="en">
+            <Html lang="en">
             <Head>
                 <meta charSet="utf-8"/>
                 {/* Use minimum-scale=1 to enable GPU rasterization */}
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-                />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
                       integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossOrigin="anonymous"/>
                 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Roboto&display=swap" rel="stylesheet" />
@@ -20,7 +16,7 @@ class MyDocument extends Document {
             <Main/>
             <NextScript/>
             </body>
-            </html>);
+            </Html>);
     }
 }
 
