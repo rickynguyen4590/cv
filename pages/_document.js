02d2327cc9d4be9d5c056422c6c9ccd5f7a7aba2
Ricky Nguyen <ricky.nguyen@noria.no>
2021-01-20 11:10:14 +0700

    Add gg tag for tracking who view my CV

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,18 @@ class MyDocument extends Document {
             <body>
             <Main/>
             <NextScript/>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-0QNC50F54M"/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-0QNC50F54M');
+        `,
+                }}
+            />
             </body>
             </Html>);
     }
